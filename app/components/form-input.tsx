756147289070fe9b0641dc4d04d0d17be08653a7
yongveloper <dev_yong@kakao.com>
2025-07-04 18:34:02 +0900

    feat: Form Components

---
+++ app/components/form-input.tsx
@@ -0,0 +1,31 @@
+import { InputHTMLAttributes } from 'react';
+
+interface IFormInputProps {
+  type: InputHTMLAttributes<HTMLInputElement>['type'];
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+}: IFormInputProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="h-10 w-full rounded-md border-none bg-transparent px-4 ring-2 ring-neutral-200 placeholder:text-neutral-400 focus:ring-4 focus:ring-orange-500 focus:outline-none"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
