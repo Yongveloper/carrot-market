872490a61d3a673fc1222e433dec6a985b64f29c
yongveloper <dev_yong@kakao.com>
2025-09-30 11:11:59 +0900

    feat: Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -5,6 +5,7 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
 
 const checkUsername = (username: string) => !username.includes('lala');
@@ -17,6 +18,32 @@ const checkPasswords = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -26,8 +53,17 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .refine(checkUsername, 'custrom error'),
-    email: z.string().email().toLowerCase().trim(),
+      .refine(checkUsername, 'custrom error')
+      .refine(checkUniqueUsername, 'This username is already taken'),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .trim()
+      .refine(
+        checkUniqueEmail,
+        'There is an account already registered with that email.',
+      ),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
@@ -47,7 +83,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get('confirm_password') as string,
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return {
@@ -56,5 +92,10 @@ export async function createAccount(prevState: any, formData: FormData) {
     };
   }
 
-  console.log(result.data);
+  // check if username is taken
+  // check if the email is already used
+  // hash password
+  // save the user to db
+  // log the user in
+  // redirect '/home'
 }
