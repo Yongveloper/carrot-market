ccce431fb06a393c925352109517ffd04a1484e8
yongveloper <dev_yong@kakao.com>
2025-09-30 15:44:52 +0900

    feat: Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,31 @@
-'use server';
 import { NextRequest, NextResponse } from 'next/server';
+import getSession from './lib/session';
+
+interface IRoutes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: IRoutes = {
+  '/': true,
+  '/login': true,
+  '/sms': true,
+  '/create-account': true,
+};
 
 export async function middleware(request: NextRequest) {
   console.log('hello');
 
-  const { pathname } = request.nextUrl;
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
 
-  if (pathname === '/') {
-    const response = NextResponse.next();
-    response.cookies.set('middleware-cookie', 'hello');
-
-    return response;
-  }
-
-  if (pathname === '/profile') {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL('/products', request.url));
+    }
   }
 }
 
