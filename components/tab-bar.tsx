c45434e7eee519e59cce86a7d3b01cb9fb5540c8
yongveloper <dev_yong@kakao.com>
2025-10-01 14:48:08 +0900

    feat: Tab Bar

---
+++ components/tab-bar.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import {
+  NewspaperIcon as OutlineNewspaperIcon,
+  HomeIcon as OutlineHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as OutlineChatIcon,
+  VideoCameraIcon as OutlineLiveIcon,
+  UserIcon as OutlineUserIcon,
+} from '@heroicons/react/24/outline';
+import {
+  NewspaperIcon as SolidNewspaperIcon,
+  HomeIcon as SolidHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as SolidChatIcon,
+  VideoCameraIcon as SolidLiveIcon,
+  UserIcon as SolidUserIcon,
+} from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function TabBar() {
+  const pathname = usePathname();
+
+  return (
+    <div className="fixed bottom-0 mx-auto grid w-full max-w-screen-md grid-cols-5 border-t border-neutral-600 px-5 py-3 *:text-white">
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        {pathname === '/products' ? (
+          <SolidHomeIcon className="h-7 w-7" />
+        ) : (
+          <OutlineHomeIcon className="h-7 w-7" />
+        )}
+        <span>홈</span>
+      </Link>
+      <Link href="/life" className="flex flex-col items-center gap-px">
+        {pathname === '/life' ? (
+          <SolidNewspaperIcon className="h-7 w-7" />
+        ) : (
+          <OutlineNewspaperIcon className="h-7 w-7" />
+        )}
+        <span>동네생활</span>
+      </Link>
+      <Link href="/chat" className="flex flex-col items-center gap-px">
+        {pathname === '/chat' ? (
+          <SolidChatIcon className="h-7 w-7" />
+        ) : (
+          <OutlineChatIcon className="h-7 w-7" />
+        )}
+        <span>채팅</span>
+      </Link>
+      <Link href="/live" className="flex flex-col items-center gap-px">
+        {pathname === '/live' ? (
+          <SolidLiveIcon className="h-7 w-7" />
+        ) : (
+          <OutlineLiveIcon className="h-7 w-7" />
+        )}
+        <span>쇼핑</span>
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center gap-px">
+        {pathname === '/profile' ? (
+          <SolidUserIcon className="h-7 w-7" />
+        ) : (
+          <OutlineUserIcon className="h-7 w-7" />
+        )}
+        <span>나의 당근</span>
+      </Link>
+    </div>
+  );
+}
