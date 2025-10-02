9eab789e5880af05821ec0333588ca9d414f96de
yongveloper <dev_yong@kakao.com>
2025-10-02 14:14:41 +0900

    feat: Form Action

---
+++ app/products/add/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Button from '@/components/button';
+import Input from '@/components/input';
+import { PhotoIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+import { uploadProduct } from './actions';
+
+export default function AddProduct() {
+  const [preview, setPreview] = useState('');
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+
+    if (!files) {
+      return;
+    }
+
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
+
+  return (
+    <div>
+      <form action={uploadProduct} className="flex flex-col gap-5 p-5">
+        <label
+          htmlFor="photo"
+          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 bg-cover bg-center text-neutral-300"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
+        >
+          {preview === '' && (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neural-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          )}
+        </label>
+        <input
+          onChange={onImageChange}
+          type="file"
+          id="photo"
+          name="photo"
+          className="hidden"
+        />
+        <Input name="title" required placeholder="제목" type="text" />
+        <Input name="price" required placeholder="가격" type="number" />
+        <Input
+          name="description"
+          required
+          placeholder="자세한 설명"
+          type="text"
+        />
+        <Button text="작성완료" />
+      </form>
+    </div>
+  );
+}
